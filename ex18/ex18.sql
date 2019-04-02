52bd7d20b757118626b1dcc7ad53f88b86ea2238
staminhe42 <staminhe42@gmail.com>
2019-04-02 19:11:00 +0200

    correct ex18

--- ex18/ex18.sql
+++ ex18/ex18.sql
@@ -1,7 +1,7 @@
 SELECT name, id_distrib
 FROM db_staminh.distrib
-WHERE id_distrib IN (42,62,63,64,65,66,67,68,69,71,88,89,90)
-OR name LIKE '%y%y%'
-OR name LIKE '%Y%Y%'
+WHERE id_distrib IN ((42,62,63,64,65,66,67,68,69,71,88,89,90))
+AND() name LIKE '%y%y%'
+OR name LIKE '%Y%Y%')
 LIMIT 5 OFFSET 2
 ;
